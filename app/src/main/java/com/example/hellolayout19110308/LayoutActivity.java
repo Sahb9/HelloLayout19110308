d60a3726f2872127a57baaa77c1f16898afe7789
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2022-03-01 20:07:40 +0700

    first commit

---
+++ app/src/main/java/com/example/hellolayout19110308/LayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.hellolayout19110308;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
